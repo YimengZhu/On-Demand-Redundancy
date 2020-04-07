05167b14d9c0b2fbcccfcb40bae90d95c847a5c2
Yimeng Zhu <yimeng.zhu0821@gmail.com>
2020-04-07 22:14:42 +0100

    --Feature; Setup.sh to setup the enviroment, requirements.txt to install python dependency.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y python3-pip libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev qt4-dev-tools libatlas-base-dev
+
+
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
+unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -d Sample_TFLite_model
+
+python3 -m venv env
+source env/bin/activate
+
+pip install -r requirements.txt
